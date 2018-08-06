d924313a6249f25ef12858adeafafe1cf2d149a0
initLiu90 <xianyue_pan@yeah.net>
2018-08-07 00:17:09 +0800

    1.闪屏也全屏展示，为了在首次启动时不会闪现黑色状态栏，需要在AndroidManifest文件中配置全屏主题

--- app/src/main/java/com/lzp/daily/activity/SplashActivity.java
+++ app/src/main/java/com/lzp/daily/activity/SplashActivity.java
@@ -1,8 +1,13 @@
 package com.lzp.daily.activity;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.lzp.core.base.BaseActivity;
 import com.lzp.daily.R;
@@ -11,25 +16,17 @@ public class SplashActivity extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle bundle) {
         super.onCreate(bundle);
-        hideSystemUI();
         getWindow().setBackgroundDrawable(null);
         setContentView(R.layout.activity_splash);
+        hideBottomNavigation();
     }
 
-    private void hideSystemUI() {
-        // Enables regular immersive mode.
-        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
-        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+    private void hideBottomNavigation() {
         View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                        // Set the content to appear under the system bars so that the
-                        // content doesn't resize when the system bars hide and show.
                         | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        // Hide the nav bar and status bar
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
     }
 }
